f6e9db88a9c825e41ae23507aac0b2996bfbab1f
Ian Wakely <raveious.irw@gmail.com>
2024-12-02 20:11:18 -0500

    Finished part 2 of day 1!!
    Signed-off-by: Ian Wakely <raveious.irw@gmail.com>

--- 2024/day1/src/main.rs
+++ 2024/day1/src/main.rs
@@ -4,7 +4,9 @@ use std::io::{prelude::*, BufReader};
 fn main() -> std::io::Result<()>{
     let mut left: Vec<u32> = Vec::with_capacity(1000);
     let mut right: Vec<u32> = Vec::with_capacity(1000);
-    let mut sum: u32 = 0;
+    let mut right_count: Vec<u32> = vec![0; 99999];
+    let mut part1_sum: u32 = 0;
+    let mut part2_sum: u32 = 0;
 
     let file = File::open("input.txt")?;
     let reader = BufReader::new(file);
@@ -12,11 +14,13 @@ fn main() -> std::io::Result<()>{
     for line in reader.lines() {
         assert!(line.is_ok(), "Failed to read a line from the input file.");
         let line_string = line.unwrap();
-        let left_string = &line_string[0..6].trim();
-        let right_string = &line_string[8..].trim();
+        let left_val = line_string[0..6].trim().parse::<u32>().unwrap();
+        let right_val = line_string[8..].trim().parse::<u32>().unwrap();
 
-        left.push(left_string.parse::<u32>().unwrap());
-        right.push(right_string.parse::<u32>().unwrap());
+        left.push(left_val);
+        right.push(right_val);
+
+        right_count[right_val as usize] += 1;
     }
 
     println!(r"Loaded {} left entries and {} right entries!", left.len(), right.len());
@@ -28,13 +32,16 @@ fn main() -> std::io::Result<()>{
 
     for i in 0..left.len() {
         if left[i] < right[i] {
-            sum += right[i] - left[i];
+            part1_sum += right[i] - left[i];
         } else {
-            sum += left[i] - right[i];
+            part1_sum += left[i] - right[i];
         }
+
+        part2_sum += left[i] * right_count[left[i] as usize];
     }
 
-    println!(r"Answer: {}", sum);
+    println!(r"Answer 1: {}", part1_sum);
+    println!(r"Answer 2: {}", part2_sum);
 
     Ok(())
 }
