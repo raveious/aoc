2f417e62f481311c3c061c70366464ec37be0348
Ian Wakely <raveious.irw@gmail.com>
2024-12-03 07:24:19 -0500

    Day 3 part 2!!!!
    Signed-off-by: Ian Wakely <raveious.irw@gmail.com>

--- 2024/day3/src/main.rs
+++ 2024/day3/src/main.rs
@@ -1,24 +1,35 @@
 use std::fs::File;
 use std::io::Read;
-use std::io::prelude::*;
 use regex::Regex;
 
 fn main() -> std::io::Result<()> {
 
     let mut file = File::open("input.txt")?;
     let mut contents = String::new();
-    let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
-    let mut part1_sum: u32 = 0;
+    let multiply_regex = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+    let word_regex = Regex::new(r"(mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\))").unwrap();
+    let mut enabled: bool = true;
+    let mut sum: u32 = 0;
 
     file.read_to_string(&mut contents)?;
 
-    for (_, [lhs, rhs]) in re.captures_iter(&contents).map(|c| c.extract()) {
-        println!("lhs: {}  rhs: {}", lhs, rhs);
+    for (_, [word]) in word_regex.captures_iter(&contents).map(|c| c.extract()) {
+        println!("word: {}", word);
 
-        part1_sum += lhs.parse::<u32>().unwrap() * rhs.parse::<u32>().unwrap();
+        match word{
+            "do()" => enabled = true,
+            "don't()" => enabled = false,
+            &_ => (),
+        }
+
+        if enabled && &word[0..3] == "mul"{
+            let caps = multiply_regex.captures(word).unwrap();
+            println!("lhs: {}  rhs: {}", &caps[1], &caps[2]);
+            sum += caps[1].parse::<u32>().unwrap() * caps[2].parse::<u32>().unwrap();
+        }
     }
 
-    println!("Part 1 Answer: {}", part1_sum);
+    println!("Answer: {}", sum);
 
     Ok(())
 }
